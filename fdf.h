e623c490dab41721a8d45c09ed1b51bcae8475fc
VLN37 <54167882+VLN37@users.noreply.github.com>
2021-09-11 01:58:40 -0300

    chore: normie

--- fdf.h
+++ fdf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jofelipe <jofelipe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/11 01:58:02 by jofelipe          #+#    #+#             */
+/*   Updated: 2021/09/11 01:58:02 by jofelipe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FDF_H
 # define FDF_H
 
@@ -6,18 +18,19 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-#define WIDTH 800
-#define HEIGHT 600
-#define OFFSET 80
-#define INCREMENT 5
+# define WIDTH 800
+# define HEIGHT 600
+# define OFFSET 80
+# define INCREMENT 5
+
 typedef struct s_img
 {
-	void *mlx_ptr;
-	void *win_ptr;
-	void *win_img;
-	int	bbp;
-	int	size_line;
-	int	end;
+	void	*mlx_ptr;
+	void	*win_ptr;
+	void	*win_img;
+	int		bbp;
+	int		size_line;
+	int		end;
 }	t_img;
 
 #endif
